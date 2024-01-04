854fe79e018c6ada45f414daa77e9ff7f1e20d75
FAIZAN AHMED <108749861+FURIOUSCHAMP007@users.noreply.github.com>
2024-01-04 14:01:10 +0530

    Add files via upload

---
+++ JAN/GFG-POTD-24-JAN-4--Find element occuring once when all other are present thrice.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int singleElement(int[] nums , int N) {
+        // code here
+        int res=0;
+        for( int i=0; i<32;i++)
+        {
+            int ans=0;
+            for( int j =0; j<nums.length; j++){
+                if( (nums[j] & (1<<i)) !=0)
+                    ans++;   
+            }
+            if(ans%3==1)
+            res+= (1<<i);
+        }
+        return res;
+    }
+}
+    
+
